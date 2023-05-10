19970ed6a820614dff7a9f148977b164aa81f2a9
Anders Åberg <anders@andersaberg.com>
2023-05-10 11:06:14 +0200

    Support for v4
    * Added .idea
    
    * Changed API signature
    
    * Added correct object return
    
    * Tweaked the generation of dist
    
    * Made small usability improvements to the client
    
    * Formatting
    
    * Formatting
    
    * Added TokenResponse register() and removed sessionId
    
    * Imroved error handling
    
    * Enabled support for using discoverable credentials
    
    * Cleaned up error logs
    
    * Added Client-Version header
    
    * Changed apiUrl
    
    * Update package.json
    
    * Updated the package

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Webauthn Passwordless js library
 
-This library allows you to fast & without complexity add passwordless sign in (using fido2/webauthn) to your web application.
+This library allows you to fast & without complexity add passwordless sign in (using fido2/webauthn) to your web
+application.
 
 [Read the paswordless documentation](https://docs.passwordless.dev/)
 
@@ -10,13 +11,17 @@ This is what you need to do:
 
 1. [Read the docs](https://docs.passwordless.dev/)
 3. **You add our client side library** and call the function `passwordless.register` or `passwordless.signin`
-4. **You add two very simple endpoints on your backend** that integrates to your existing user system (*set cookie, sessions, etc*) (and communicates secrets with our API).
-5. You make a request between your clientside code and the verification endpoints on your backend to verify the registration or sign in.
- 
+4. **You add two very simple endpoints on your backend** that integrates to your existing user system (*set cookie,
+   sessions, etc*) (and communicates secrets with our API).
+5. You make a request between your clientside code and the verification endpoints on your backend to verify the
+   registration or sign in.
+
 ## Get coding
+
 To get started, add the library to your website (either as ES6 module or global):
 
 NPM package:
+
 ```bash
 yarn add @passwordlessdev/passwordless-client
 ```
@@ -26,6 +31,7 @@ import { Client } from '@passwordlessdev/passwordless-client';
 ```
 
 Normal script tag:
+
 ```html
 <script src="https://cdn.passwordless.dev/dist/0.3.0/passwordless.iife.min.js" crossorigin="anonymous"></script>
 
@@ -35,16 +41,19 @@ var p = new Passwordless.Client({});
 ```
 
 ES6 module script-tag:
+
 ```html
 <script src="https://cdn.passwordless.dev/dist/0.3.0/passwordless.min.mjs" crossorigin="anonymous"></script>
 ```
 
 ES6 module:
+
 ```js
 import { Client } from "https://cdn.passwordless.dev/dist/0.3.0/passwordless.min.mjs"
 ```
 
 UMD module:
+
 ```
 https://cdn.passwordless.dev/dist/0.3.0/passwordless.umd.min.js
 ```
--- rollup.config.js
+++ rollup.config.js
@@ -2,49 +2,54 @@ import {terser} from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 
 const createPlugins = (compact) => compact
-  ? [terser()]
-  : [];
+    ? [terser()]
+    : [];
 
 const iife = (compact) => ({
-  file: `dist/passwordless.iife${compact ? '.min' : ''}.js`,
-  format: 'iife',
-  name: 'Passwordless',
-  sourcemap: true,
-  plugins: createPlugins(compact),
+    file: `dist/iife/passwordless.iife${compact ? '.min' : ''}.js`,
+    format: 'iife',
+    name: 'Passwordless',
+    sourcemap: true,
+    exports: "named",
+    plugins: createPlugins(compact),
 });
 
 const es6 = (compact) => ({
-  file: `dist/passwordless${compact ? '.min' : ''}.mjs`,
-  format: 'es',
-  sourcemap: true,
-  plugins: createPlugins(compact),
+    file: `dist/esm/passwordless${compact ? '.min' : ''}.mjs`,
+    format: 'esm',
+    sourcemap: true,
+    exports: "named",
+    plugins: createPlugins(compact),
 });
 
 const umd = (compact) => ({
-  file: `dist/passwordless.umd${compact ? '.min' : ''}.js`,
-  format: 'umd',
-  name: 'Passwordless',
-  plugins: createPlugins(compact),
+    file: `dist/umd/passwordless.umd${compact ? '.min' : ''}.js`,
+    format: 'umd',
+    name: 'Passwordless',
+    esModule: false,
+    exports: 'named',
+    plugins: createPlugins(compact),
 });
 
 const cjs = (compact) => ({
-  file: `dist/passwordless${compact ? '.min' : ''}.js`,
-  format: 'cjs',
-  name: 'Passwordless',
-  plugins: createPlugins(compact),
+    file: `dist/cjs/passwordless${compact ? '.min' : ''}.js`,
+    format: 'cjs',
+    name: 'Passwordless',
+    exports: "named",
+    plugins: createPlugins(compact),
 });
 
 export default {
-  input: 'src/passwordless.ts',
-  plugins: [typescript()],
-  output: [
-    iife(false),
-    iife(true),
-    es6(false),
-    es6(true),
-    umd(false),
-    umd(true),
-    cjs(false),
-    cjs(true),
-  ],
+    input: 'src/passwordless.ts',
+    plugins: [typescript()],
+    output: [
+        iife(false),
+        iife(true),
+        es6(false),
+        es6(true),
+        umd(false),
+        umd(true),
+        cjs(false),
+        cjs(true),
+    ],
 };
