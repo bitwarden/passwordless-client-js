8ae0a5d0ddfc922a3dc499320e66043fd1b55821
Jared McCannon <jmccannon@bitwarden.com>
2026-05-07 15:58:22 -0500

    ignoring public js files.

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ export default [
   js.configs.recommended,
   eslintConfigPrettier,
   {
-    ignores: ['node_modules/'],
+    ignores: ['node_modules/', 'examples/simple-example/public/*.js'],
     rules: {
       'no-unused-vars': 'warn'
     }
